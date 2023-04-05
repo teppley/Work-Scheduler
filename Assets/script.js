3e47d5eacbe496c9b398ccb2d50d2078857d8041
Tyler Eppley <teppley300@gmail.com>
2023-04-05 18:43:10 -0400

    added JS to change color of time blocks

--- Assets/script.js
+++ Assets/script.js
@@ -9,6 +9,26 @@ $(document).ready(function() {
   $("#current-date").text(formattedDate);
 });
 
+//Sets color of row based off current time
+const currentHour = new Date().getHours();
+const timeBlocks = document.querySelectorAll('.time-block');
+timeBlocks.forEach(timeBlock => {
+  
+// Get the hour from the id attribute of the div
+const hour = parseInt(timeBlock.id.split('-')[1]);
+
+// Add the appropriate class based on the comparison to the current hour
+  if (hour < currentHour) {
+    timeBlock.classList.add('past');
+  } else if (hour === currentHour) {
+    timeBlock.classList.add('present');
+  } else {
+    timeBlock.classList.add('future');
+    $(this).removeClass('past');
+    $(this).removeClass('present');
+  }
+});
+
 
 
 
@@ -17,7 +37,11 @@ $(document).ready(function() {
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
+  
+});
+
+
+// TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -35,4 +59,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
